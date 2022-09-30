ba51ebcd6c17d667f57ce07ed4792b8aceb5debb
Steffie Ann Thayil <sthayil@cern.ch>
2022-09-30 07:41:18 -0500

    greatly improved usability

--- makeHadd.py
+++ makeHadd.py
@@ -1,9 +1,31 @@
 import os, sys
+import glob, argparse, socket, time
+options = argparse.ArgumentParser(description="Find missing files")
+options.add_argument("-i", "--inputDir", required=True, help="Dir to check")
+options.add_argument("-o", "--outputFilename", required=True, help="name of output file (without .root)")
+ops = options.parse_args()
 
-badfiles=[]
-command='python ../scripts/haddnano.py '+sys.argv[4]+'_'+sys.argv[1]
+filenums=[]
+os.system("ls "+ops.inputDir+" > filelist")
+dirfiles=open('filelist','r')
+for line in dirfiles.readlines():
+    if line.startswith('NANOAOD'): 
+        filenum=int(filter(str.isdigit, line))
+        filenums.append(filenum)
+dirfiles.close()
 
-for i in range(int(sys.argv[2]), int(sys.argv[3])):
-    if i not in badfiles: command+=' NANOAOD_TwoProng_'+str(i)+'_Skim.root'
+print "num of files: ", len(filenums)
+print "max filenum: ", max(filenums)
 
+for j in range(0,int(max(filenums)/1000)+1):
+    command='python scripts/haddnano.py '+str(j)+'_'+ops.outputFilename+'.root '
+    for k in range(0,1000):
+        if (1000*j)+k in filenums: command+=ops.inputDir+'/NANOAOD_TwoProng_'+str((1000*j)+k)+'_Skim.root '
+    os.system(command)
+
+command='python scripts/haddnano.py '+ops.outputFilename+'.root '
+for j in range(0,int(max(filenums)/1000)+1): command+=str(j)+'_'+ops.outputFilename+'.root '
 os.system(command)
+
+for j in range(0,int(max(filenums)/1000)+1): os.system('rm '+str(j)+'_'+ops.outputFilename+'.root ')
+os.system("rm filelist")
--- missingFiles.py
+++ missingFiles.py
@@ -2,9 +2,17 @@ import sys, os
 
 #usage: python missingfiles.py /eos/uscms/store/user/lpcrutgers/sthayil/pseudoaxions/ttPhiPS_M-250/eosdirtocheck NumOfExpectedFilesEg10
 
-#os.system("eos root://cmseos.fnal.gov ls "+sys.argv[1]+" > filelist")
-os.system("ls "+sys.argv[1]+" > filelist")
+import glob, argparse, socket
+options = argparse.ArgumentParser(description="Find missing files")
+options.add_argument("-m", "--mode", required=True, help="eos or local?", choices=['eos','local'])
+options.add_argument("-i", "--inputDir", required=True, help="Dir to check")
+ops = options.parse_args()
+
 filenums=[]
+
+if ops.mode == 'eos': os.system("eos root://cmseos.fnal.gov ls "+ops.inputDir+" > filelist")
+elif ops.mode == 'local': os.system("ls "+ops.inputDir+" > filelist")
+
 dirfiles=open('filelist','r')
 for line in dirfiles.readlines():
     if line.startswith('NANOAOD'): #to check for miniAOD_*.root, for example
@@ -13,8 +21,9 @@ for line in dirfiles.readlines():
 dirfiles.close()
 
 print "num of files: ", len(filenums)
+print "max filenum: ", max(filenums)
 
-for i in range(0, int(sys.argv[2])):
+for i in range(0, max(filenums)+1):
     if i not in filenums: print str(i)+",",
 
 os.system("rm filelist")
