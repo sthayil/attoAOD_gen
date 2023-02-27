6a851c0f434c7ab2e7de54ba2d849ddb3a1ff0f2
Steffie Ann Thayil <sthayil@cern.ch>
2023-02-27 12:44:04 -0500

    temp removal of all selections (muon, twoprong, trig)

--- python/postprocessing/modules/attoAOD_ttw_mu.py
+++ python/postprocessing/modules/attoAOD_ttw_mu.py
@@ -10,6 +10,7 @@ from datetime import datetime
 #11:/eos/uscms/store/user/lpcrutgers/sthayil/pseudoaxions/nano/ttPhiPS_M-1000/
 #12:/eos/uscms/store/user/lpcrutgers/sthayil/pseudoaxions/nano/ttPhiPS_M-500/
 #13:/eos/uscms/store/user/lpcrutgers/sthayil/pseudoaxions/nano/ttPhiPS_M-250/
+#21: /TTJets_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM
 #21: /WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM  
 #22: /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM
 
@@ -17,9 +18,10 @@ from datetime import datetime
 #221007: require that events pass trigger to be saved into atto (to reduce filesizes)
 #221013: split el and mu modules
 #221209: undo trig req
+#230227: undo all selections (no twoprong, lep or trig)
 
 class attoAOD_ttw_mu(Module):
-    def __init__(self, year="2018", mctype="0", attoVersion="221209"): 
+    def __init__(self, year="2018", mctype="0", attoVersion="230227"): 
         self.year = year
         self.mctype = mctype
         self.attoVersion = attoVersion
@@ -79,21 +81,21 @@ class attoAOD_ttw_mu(Module):
             #return False #temporary
 
         #event selection
-        goodTwoprong=False
-        if len(twoprongs)<1: return False
-        for twoprong in twoprongs:
-            if twoprong.pt>20 and abs(twoprong.eta)<2.5: 
-                goodTwoprong=True
-                break
-        if goodTwoprong==False: return False
+        # goodTwoprong=False
+        # if len(twoprongs)<1: return False
+        # for twoprong in twoprongs:
+        #     if twoprong.pt>20 and abs(twoprong.eta)<2.5: 
+        #         goodTwoprong=True
+        #         break
+        # if goodTwoprong==False: return False
 
-        goodMuon=False
-        if len(muons)<1: return False
-        for muon in muons:
-            if muon.pt>52 and abs(muon.eta)<2.4: 
-                goodMuon = True
-                break
-        if goodMuon==False: return False
+        # goodMuon=False
+        # if len(muons)<1: return False
+        # for muon in muons:
+        #     if muon.pt>52 and abs(muon.eta)<2.4: 
+        #         goodMuon = True
+        #         break
+        # if goodMuon==False: return False
 
         #fill branches
         self.out.fillBranch("year", int(self.year))
